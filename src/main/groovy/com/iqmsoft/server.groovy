4a7d56058f09a0d7841d249a71bf08df2c55f2a2
Davanon <davanon2014@gmail.com>
2018-02-24 09:25:29 +0530

    First

---
+++ src/main/groovy/com/iqmsoft/server.groovy
@@ -0,0 +1,13 @@
+package com.iqmsoft
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ServerApplication {
+
+    static void main(String[] args) throws Exception {
+		SpringApplication.run(ServerApplication.class, args)
+	}
+
+}
